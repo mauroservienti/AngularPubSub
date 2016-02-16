b4aac154f2d887a57f2e66961db5b08431ede0eb
Mauro Servienti <mauro@topics.it>
2016-02-16 09:40:57 +0100

    app startup process

--- app/app.js
+++ app/app.js
@@ -3,56 +3,28 @@
 
     'use strict';
 
-    angular.module('composite.ui.app.controllers', []);
-    angular.module('composite.ui.app.services', []);
+    angular.module('app.controllers', []);
+    angular.module('app.services', []);
 
     var app = angular.module('app', [
-                'ngRoute',
-                'ngAnimate',
-                'ui.router',
-                'ui.bootstrap',
-                'cgBusy',
-                'radical.itemTemplate',
-                'composite.ui.app.controllers',
-                'composite.ui.app.services',
-                'composite.ui.app.templates'
+                'app.controllers',
+                'app.services',
+                'app.templates'
     ]);
-
-    app.config(['$stateProvider', '$locationProvider', '$logProvider', 'radicalItemTemplateConfigProvider',
-            function ($stateProvider, $locationProvider, $logProvider, itemTemplateConfigProvider) {
-
+    
+    app.config(['$locationProvider', '$logProvider',
+            function ($locationProvider, $logProvider) {
                 $logProvider.debugEnabled(true);
                 $locationProvider.html5Mode(false);
-                itemTemplateConfigProvider.setDefaultSettings({
-                    templatesFolder: '/app/composition/templates/'
-                });
-
-                var rootViews = {
-                    '': {
-                        templateUrl: '/app/presentation/dashboardView.html',
-                        controller: 'dashboardController as dashboard'
-                    }
-                };
-
-                $stateProvider
-                    .state('root', {
-                        url: '',
-                        views: rootViews
-                    })
-                    .state('dashboard', {
-                        url: '/',
-                        views: rootViews
-                    });
-
+                
+                console.debug('app config.');
             }]);
 
-    app.run(['$log', '$rootScope', '$state', '$stateParams',
-        function ($log, $rootScope, $state, $stateParams) {
-
-            $rootScope.$state = $state;
+    app.run(['$log', '$rootScope',
+        function ($log, $rootScope) {
             $rootScope.$log = $log;
-            $rootScope.$stateParams = $stateParams;
-
+            
+            console.debug('app run.');
         }]);
 
 }())
